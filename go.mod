bd9bf160833e6d97597f163bde9da3cda68e8336
Preetham <preetham.s@yellowmessenger.com>
2022-02-01 21:46:21 +0530

    Custom Webhook

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/preethamsathyamurthy/alertmanager-webhook-lambda
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0
+	github.com/prometheus/alertmanager v0.23.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/parnurzeal/gorequest v0.2.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.30.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
+	github.com/shurcooL/vfsgen v0.0.0-20200824052919-0d455de96546 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	google.golang.org/protobuf v1.26.0-rc.1 // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/parnurzeal/gorequest"
+	"github.com/prometheus/alertmanager/template"
+)
+
+type SlackText struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+type SlackBlock struct {
+	Type string    `json:"type"`
+	Text SlackText `json:"text"`
+}
+
+type Attachments struct {
+	Blocks []SlackBlock `json:"blocks"`
+}
+
+type MinimalSlackPayload struct {
+	Blocks  []SlackBlock `json:"blocks"`
+	Text    string       `json:"text"`
+	Channel string       `json:"channel"`
+	Icon    string       `json:"icon_emoji"`
+}
+
+var SlackChannel string
+
+func redirectPolicyFunc(req gorequest.Request, via []gorequest.Request) error {
+	return fmt.Errorf("Incorrect token (redirection)")
+}
+
+// This function sends the slack message
+func Send(webhookUrl string, proxy string, payload MinimalSlackPayload) []error {
+	request := gorequest.New().Proxy(proxy)
+	resp, _, err := request.
+		Post(webhookUrl).
+		RedirectPolicy(redirectPolicyFunc).
+		Send(payload).
+		End()
+
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode >= 400 {
+		return []error{fmt.Errorf("Error sending msg. Status: %v", resp.Status)}
+	}
+
+	return nil
+}
+
+func expandAlerts(alert template.Alert) SlackText {
+	alertName := alert.Labels["alertname"]
+	fmt.Println(alertName)
+	slackText := SlackText{}
+	slackText.Type = "mrkdwn"
+	slackText.Text = "*Alert: * " + alertName
+	slackText.Text += "\n*Status:* " + alert.Status
+	slackText.Text += "\n*Summary:* " + alert.Annotations["summary"]
+	slackText.Text += "\n*Description:* " + alert.Annotations["description"]
+	slackText.Text += "\n*StartsAt:* _" + alert.StartsAt.String() + "_"
+	slackText.Text += "\n*EndsAt:* _" + alert.EndsAt.String() + "_"
+	slackText.Text += "\n\n*Details:* "
+	for k, v := range alert.Labels {
+		slackText.Text += fmt.Sprintf("\n- *_%s_* : `%s`", k, v)
+	}
+	slackText.Text += "\n\n*GeneratorURL:* " + alert.GeneratorURL
+	return slackText
+}
+
+// This function generates the SLACK payload
+func generateSlackMessage(data *template.Data, slackChannel string) MinimalSlackPayload {
+	minimalSlackPayload := MinimalSlackPayload{}
+	minimalSlackPayload.Icon = ":alert:"
+	minimalSlackPayload.Channel = slackChannel
+	slackBlockHeader := SlackBlock{}
+	slackBlockHeader.Type = "header"
+	slackBlockHeader.Text = SlackText{
+		Type: "plain_text",
+		Text: fmt.Sprintf("%d Alerts received", len(data.Alerts)),
+	}
+	minimalSlackPayload.Blocks = append(minimalSlackPayload.Blocks, slackBlockHeader)
+
+	for i := 0; i < len(data.Alerts); i++ {
+		slackBlock := SlackBlock{}
+		slackBlock.Type = "section"
+		slackBlock.Text = expandAlerts(data.Alerts[i])
+		minimalSlackPayload.Blocks = append(minimalSlackPayload.Blocks, slackBlock)
+	}
+	return minimalSlackPayload
+}
+
+// This function handles the SLACK request
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	data := new(template.Data) // creating a template for reponse payload of alertmanager
+	ApiResponse := events.APIGatewayProxyResponse{}
+
+	err := json.Unmarshal([]byte(request.Body), &data)
+	if err != nil {
+		body := "Error: Invalid JSON payload ||| " + fmt.Sprint(err) + " Body Obtained" + "||||" + request.Body
+		ApiResponse = events.APIGatewayProxyResponse{Body: body, StatusCode: 500}
+	} else {
+		minimalSlackPayload := generateSlackMessage(data, request.QueryStringParameters["channel"])
+		sendSlackError := Send("https://hooks.slack.com/services/", "", minimalSlackPayload) // Update the appropriate Slack token here
+		if sendSlackError != nil {
+			fmt.Println("Error sending Slack message with error ")
+			fmt.Print(sendSlackError)
+			fmt.Println("######################################")
+			ApiResponse = events.APIGatewayProxyResponse{Body: "Error sending Slack message", StatusCode: 502}
+		}
+		v, _ := json.Marshal(minimalSlackPayload)
+		ApiResponse = events.APIGatewayProxyResponse{Body: string(v), StatusCode: 200}
+	}
+	fmt.Printf(ApiResponse.Body)
+	return ApiResponse, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
